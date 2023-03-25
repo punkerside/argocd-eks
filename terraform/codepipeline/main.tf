2147edc08d7c5b1cf99b65a8c45939b00a319ce2
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-03-25 02:40:03 -0500

    agregando privileged_mode

--- terraform/codepipeline/main.tf
+++ terraform/codepipeline/main.tf
@@ -187,10 +187,10 @@ resource "aws_codebuild_project" "main" {
   }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
-    type                        = "LINUX_CONTAINER"
-    # image_pull_credentials_type = "SERVICE_ROLE"
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
   }
 
   source {
