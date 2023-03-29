79b197d634e5a348d716fa1953258d15e90eaef4
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-03-28 22:32:19 -0500

    quitando latest

--- script/build.sh
+++ script/build.sh
@@ -24,6 +24,4 @@ docker run --rm -u $DOCKER_UID:$DOCKER_GID -v $PWD/passwd:/etc/passwd:ro -v $PWD
 # release
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com
 docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT-$ENV-$SERVICE:$TAG_RELEASE --build-arg IMG=$PROJECT-$ENV:base -f docker/$SERVICE/latest/Dockerfile .
-docker tag $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT-$ENV-$SERVICE:$TAG_RELEASE $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT-$ENV-$SERVICE:latest
 docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT-$ENV-$SERVICE:$TAG_RELEASE
-docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT-$ENV-$SERVICE:latest
