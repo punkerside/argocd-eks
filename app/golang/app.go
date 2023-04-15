d262197815e97dbb9a59c421eea7ae24f1c311aa
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-04-15 11:43:41 -0500

    nueva version de imagen

--- app/golang/app.go
+++ app/golang/app.go
@@ -23,7 +23,7 @@ func main() {
 
 // funcion de estado
 func getHome(w http.ResponseWriter, r *http.Request) {
-    raw := json.RawMessage(`{"api":"music","version":"v0.0.10"}`)
+    raw := json.RawMessage(`{"api":"music","version":"v0.0.11"}`)
     jsonResp, err := json.Marshal(&raw)
     if err != nil {
         panic(err)
